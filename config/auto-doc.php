c2a2984525dc3e08576ad609fa1391eb8eb09b88
ameteshov <ameteshov@ronasit.com>
2017-02-01 17:45:50 +0600

    #8: add support security to config

--- config/auto-doc.php
+++ config/auto-doc.php
@@ -32,6 +32,7 @@ return [
     'basePath' => '/',
     'schemes' => [],
     'definitions' => [],
+    'security' => [], //possible values : jwt, laravel, oauth
     'defaults' => [
         'code-descriptions' => [
             '200' => 'Operation successfully done',
