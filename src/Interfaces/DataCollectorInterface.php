830bcf1362642984e62b551ac8818f9616f8c51f
Roman Dubrovin <rdubrovin@ronasit.com>
2017-04-10 16:25:45 +0600

    #-: added annotations to DataCollectorInterface

--- src/Interfaces/DataCollectorInterface.php
+++ src/Interfaces/DataCollectorInterface.php
@@ -11,12 +11,28 @@ namespace RonasIT\Support\AutoDoc\Interfaces;
 
 interface DataCollectorInterface
 {
+    /**
+     * Save temporary data
+     *
+     * @param array $data
+     */
     public function saveTmpData($data);
 
+    /**
+     * Get temporary data
+     */
     public function getTmpData();
 
+    /**
+     * Save production data
+     *
+     * @param array $data
+     */
     public function saveData($data);
 
+    /**
+     * Get production documentation
+     */
     public function getDocumentation();
 }
 
