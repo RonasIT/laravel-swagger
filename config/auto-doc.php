c6279166d0c372f5e5c9e20db6b2dc65b6870fc8
Pankratov Denis <dpankratov@ronasit.com>
2018-11-23 13:39:30 +0600

    #18: добавлены комментарии к параметрам конфига

--- config/auto-doc.php
+++ config/auto-doc.php
@@ -1,15 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: roman
- * Date: 26.08.16
- * Time: 11:49
- */
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Documentation Route
+    |--------------------------------------------------------------------------
+    |
+    | Route which will return documentation
+    */
+
     'route' => '/',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Info block
+    |--------------------------------------------------------------------------
+    |
+    | Information fields
+    */
+
     'info' => [
-        'description' => 'swagger-description', // Name of view
+
+        /*
+        |--------------------------------------------------------------------------
+        | Documentation Template
+        |--------------------------------------------------------------------------
+        |
+        | You can use your custom documentation view
+        */
+
+        'description' => 'swagger-description',
         'version' => '0.0.0',
         'title' => 'Name of Your Application',
         'termsOfService' => '',
@@ -27,15 +48,43 @@ return [
     'basePath' => '/',
     'schemes' => [],
     'definitions' => [],
-    'security' => '', //possible values : jwt, laravel or null if your project dont need auth
+
+    /*
+    |--------------------------------------------------------------------------
+    | Security Library
+    |--------------------------------------------------------------------------
+    |
+    | Library name, which used to secure the project.
+    | Available values: "jwt", "laravel", "null"
+    */
+
+    'security' => '',
     'defaults' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Descriptions of code statuses
+        |--------------------------------------------------------------------------
+        */
+
         'code-descriptions' => [
             '200' => 'Operation successfully done',
             '204' => 'Operation successfully done',
             '404' => 'This entity not found'
         ]
     ],
-    // you can use your own data collector class, just write it way below,
-    // e.g App\Path-to-your-data-collector-class
+
+    /*
+    |--------------------------------------------------------------------------
+    | Data Collector Class
+    |--------------------------------------------------------------------------
+    |
+    | Class of data collector, which will collect and save documentation
+    | It can be your own data collector, or our data collectors from next libraries:
+    |
+    | ronasit/local-data-collector
+    | ronasit/laravel-remote-data-collector
+    */
+
     'data_collector' => ''
 ];
