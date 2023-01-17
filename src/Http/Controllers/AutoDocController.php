1a00ad0e73ae94510e53ca5fb784a54c7e9e56b7
dpankratov <dpankratov@ronasit.com>
2023-01-17 12:03:47 +0600

    fix: fix path to swagger assets files.

--- src/Http/Controllers/AutoDocController.php
+++ src/Http/Controllers/AutoDocController.php
@@ -36,7 +36,7 @@ class AutoDocController extends BaseController
 
     public function getFile(Request $request, $file)
     {
-        $filePath = base_path("vendor/ronasit/laravel-swagger/src/Views/swagger/{$file}");
+        $filePath = base_path("vendor/ronasit/laravel-swagger/resources/assets/swagger/{$file}");
 
         if (!file_exists($filePath)) {
             throw new NotFoundHttpException();
