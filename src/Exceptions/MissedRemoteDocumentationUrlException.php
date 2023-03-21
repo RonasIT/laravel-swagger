7e3b8298bd5999c5da72623fd31f11f4d5bb1123
Vitaly Grams <vgrams@ronasit.com>
2023-03-21 15:48:02 +0600

    fix: resolved review remarks;

--- src/Exceptions/MissedRemoteDocumentationUrlException.php
+++ src/Exceptions/MissedRemoteDocumentationUrlException.php
@@ -8,7 +8,7 @@ class MissedRemoteDocumentationUrlException extends Exception
 {
     public function __construct($message = null, $code = 0, Exception $previous = null)
     {
-        $message = $message ?? 'Remote documentation url missed in config';
+        $message = $message ?? 'Remote documentation url missed in config. Please set SWAGGER_REMOTE_DRIVER_URL env variable to define this one.';
 
         parent::__construct($message, $code, $previous);
     }
