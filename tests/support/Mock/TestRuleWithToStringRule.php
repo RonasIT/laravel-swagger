03629814b5944afeb7ec7fdc840a539718f42fd1
Nikita Yakovlev <nyakovlev@ronasit.com>
2023-06-08 18:21:42 +0600

    #87: Fix naming

--- tests/support/Mock/TestRuleWithToStringRule.php
+++ tests/support/Mock/TestRuleWithToStringRule.php
@@ -4,9 +4,14 @@ namespace RonasIT\Support\Tests\Support\Mock;
 
 use Closure;
 
-class TestRuleWithoutToArrayRule
+class TestRuleWithToStringRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
+    }
+
+    public function __toString()
+    {
+        return 'with_to_array_rule_string_name';
     }
 }
--- tests/support/Mock/TestRuleWithoutToStringRule.php
+++ tests/support/Mock/TestRuleWithoutToStringRule.php
@@ -4,14 +4,9 @@ namespace RonasIT\Support\Tests\Support\Mock;
 
 use Closure;
 
-class TestRuleWithToArrayRule
+class TestRuleWithoutToStringRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-    }
-
-    public function __toString()
-    {
-        return 'with_to_array_rule_string_name';
     }
 }
