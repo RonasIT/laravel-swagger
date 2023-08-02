733761a53aa4410e0e544bbca3933b06b6baeed8
Nikita Yakovlev <nyakovlev@ronasit.com>
2023-08-02 17:19:58 +0600

    fix: html attributes

--- resources/views/documentation-rapidoc.blade.php
+++ resources/views/documentation-rapidoc.blade.php
@@ -1,8 +1,9 @@
 <!doctype html> <!-- Important: must specify -->
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8"> <!-- Important: rapi-doc uses utf8 characters -->
         <script src="{{ config('auto-doc.global_prefix') }}/auto-doc/rapidoc-min.js"></script>
+        <title>Documentation</title>
     </head>
     <body>
         <rapi-doc
