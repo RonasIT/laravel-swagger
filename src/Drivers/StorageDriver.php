aa0984bba8fa08147789815d5958e6598fc52c91
Adil <aausagit@ronasit.com>
2024-06-05 07:36:58 +0600

    feat: logic for storage driver added, refactorin needed

--- src/Drivers/StorageDriver.php
+++ src/Drivers/StorageDriver.php
@@ -3,8 +3,10 @@
 namespace RonasIT\Support\AutoDoc\Drivers;
 
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Support\Facades\Storage;
 use RonasIT\Support\AutoDoc\Exceptions\MissedProductionFilePathException;
+use RonasIT\Support\AutoDoc\Services\SwaggerService;
 
 class StorageDriver extends BaseDriver
 {
@@ -23,9 +25,41 @@ class StorageDriver extends BaseDriver
         }
     }
 
+    private function addPaths(array &$data): void
+    {
+        foreach ($this->getTmpData()['paths'] as $tmpPathKey => $tmpPathValue) {
+            $data['paths'][$tmpPathKey] = array_key_exists($tmpPathKey, $data['paths'])
+                ? array_merge($data['paths'][$tmpPathKey], $tmpPathValue)
+                : $tmpPathValue;
+        }
+    }
+
+    private function addDefinitions(array &$data): void
+    {
+        foreach ($this->getTmpData()['definitions'] as $tmpDefKey => $tmpDefValue) {
+            $data['definitions'][$tmpDefKey] = $tmpDefValue;
+        }
+    }
+
     public function saveData(): void
     {
-        $this->disk->put($this->prodFilePath, json_encode($this->getTmpData()));
+        if (ParallelTesting::token()) {
+            if (!$this->disk->exists($this->prodFilePath)) {
+                $emptyData = app(SwaggerService::class)->generateEmptyData();
+
+                $this->disk->put($this->prodFilePath, json_encode($emptyData));
+            }
+
+            $prodFileJsonContent = $this->disk->get($this->prodFilePath);
+            $prodFileArrayContent = json_decode($prodFileJsonContent, true);
+
+            $this->addPaths($prodFileArrayContent);
+            $this->addDefinitions($prodFileArrayContent);
+
+            $this->disk->put($this->prodFilePath, json_encode($prodFileArrayContent));
+        } else {
+            $this->disk->put($this->prodFilePath, json_encode($this->getTmpData()));
+        }
 
         $this->clearTmpData();
     }
