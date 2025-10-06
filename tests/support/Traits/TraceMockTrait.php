9cc86e5b98e1efd6c339ed37399fbda4386d491f
Ruslan Guskov <feniksuh@gmail.com>
2025-10-06 18:08:01 +0300

    style:fix

--- tests/support/Traits/TraceMockTrait.php
+++ tests/support/Traits/TraceMockTrait.php
@@ -16,7 +16,7 @@ trait TraceMockTrait
             callback: fn ($value, $key) => Str::containsAll($value, ['args', 'class'])
         );
 
-        $traceInfoInArray = $this->gerTraceInfoInArray($traceInfo);
+        $traceInfoInArray = $this->getTraceInfoInArray($traceInfo);
 
         $mockedContent = Arr::set(
             array: $contentInArray,
@@ -27,7 +27,7 @@ trait TraceMockTrait
         $content = implode(PHP_EOL, $mockedContent);
     }
 
-    protected function gerTraceInfoInArray(string $traceInfo): array
+    protected function getTraceInfoInArray(string $traceInfo): array
     {
         $errorPlaceInArray = explode(', ', $traceInfo);
         $errorPlaceInArray = array_combine(
