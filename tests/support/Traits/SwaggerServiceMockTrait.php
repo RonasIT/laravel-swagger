5e8c8a1132217bfc7be1af569eb3499537018137
Ruslan Guskov <feniksuh@gmail.com>
2025-10-27 12:15:47 +0300

    fix: remove useless

--- tests/support/Traits/SwaggerServiceMockTrait.php
+++ tests/support/Traits/SwaggerServiceMockTrait.php
@@ -72,8 +72,6 @@ trait SwaggerServiceMockTrait
 
     protected function assertExceptionHTMLEqualsFixture(string $fixture, string $content, bool $exportMode = false): void
     {
-        $content  = Str::replaceMatches('/MockObject/', 'MockClass', $content);
-
         $content =  Str::replaceMatches('/line=\d+, /', 'line=999, ', $content);
 
         $content = Str::replaceMatches('/file=.*?(src\/[^\s]+)/', 'file=/$1', $content);
