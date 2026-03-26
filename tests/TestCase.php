453f96691dddbd52e6ca53f27b256b3f41ca2daa
yburlakov <yburlakov@ronasit.com>
2026-03-26 16:34:16 +0300

    chore: tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -123,13 +123,12 @@ class TestCase extends BaseTest
     protected function generateRequest(
         string $type,
         string $uri,
-        array  $data = [],
-        array  $pathParams = [],
-        array  $headers = [],
-        array  $routeConditions = [],
+        array $data = [],
+        array $pathParams = [],
+        array $headers = [],
+        array $routeConditions = [],
         string $controllerMethod = 'test',
-    ): Request
-    {
+    ): Request {
         $request = $this->getBaseRequest($type, $uri, $data, $pathParams, $headers);
 
         return $request->setRouteResolver(function () use ($uri, $request, $controllerMethod, $routeConditions) {
