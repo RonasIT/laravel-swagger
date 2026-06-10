76e2bb24ac1fb37adab1bbabb60502483952bc33
Anton Zabolotnikov <azabolotnikov@ronasit.com>
2026-06-10 11:08:56 +0500

    feat: name response object by resource class
    refs: https://github.com/RonasIT/laravel-swagger/issues/192

--- src/Inspectors/ClassControllerInspector.php
+++ src/Inspectors/ClassControllerInspector.php
@@ -3,13 +3,9 @@
 namespace RonasIT\AutoDoc\Inspectors;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Arr;
 use ReflectionException;
 use ReflectionMethod;
-use ReflectionNamedType;
-use ReflectionUnionType;
 use RonasIT\AutoDoc\Contracts\ControllerInspectorContract;
 use RonasIT\AutoDoc\DTO\ResolvedResource;
 use RonasIT\AutoDoc\Support\Resolvers\MethodDependencyResolver;
@@ -29,9 +25,7 @@ class ClassControllerInspector implements ControllerInspectorContract
     {
         $reflectionMethod = $this->getReflectionMethod();
 
-        return $reflectionMethod
-            ? $this->resolveFromReturnType($reflectionMethod->getReturnType()) ?? $this->resourceClassResolver->resolve($reflectionMethod)
-            : null;
+        return (!empty($reflectionMethod)) ? $this->resourceClassResolver->resolve($reflectionMethod) : null;
     }
 
     private function getReflectionMethod(): ?ReflectionMethod
@@ -41,23 +35,6 @@ class ClassControllerInspector implements ControllerInspectorContract
         } catch (ReflectionException) {
             return null;
         }
-    }
-
-    private function resolveFromReturnType(mixed $returnType): ?ResolvedResource
-    {
-        if ($returnType instanceof ReflectionNamedType && $this->isResourceClass($returnType->getName())) {
-            return new ResolvedResource($returnType->getName());
-        }
-
-        if ($returnType instanceof ReflectionUnionType) {
-            foreach ($returnType->getTypes() as $type) {
-                if ($this->isConcreteResourceType($type)) {
-                    return new ResolvedResource($type->getName());
-                }
-            }
-        }
-
-        return null;
     }
 
     public function getRequestClass(): ?string
@@ -72,18 +49,5 @@ class ClassControllerInspector implements ControllerInspectorContract
         );
 
         return Arr::first($parameters, fn ($className) => is_string($className) && is_subclass_of($className, FormRequest::class));
-    }
-
-    private function isConcreteResourceType(mixed $type): bool
-    {
-        return $type instanceof ReflectionNamedType
-            && !$type->isBuiltin()
-            && $this->isResourceClass($type->getName());
-    }
-
-    private function isResourceClass(string $className): bool
-    {
-        return is_subclass_of($className, JsonResource::class)
-            && $className !== AnonymousResourceCollection::class;
     }
 }
--- src/Support/Resolvers/ResourceClassResolver.php
+++ src/Support/Resolvers/ResourceClassResolver.php
@@ -2,15 +2,41 @@
 
 namespace RonasIT\AutoDoc\Support\Resolvers;
 
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use ReflectionFunctionAbstract;
+use ReflectionNamedType;
+use ReflectionUnionType;
 use RonasIT\AutoDoc\DTO\ResolvedResource;
 
 class ResourceClassResolver
 {
     public function resolve(ReflectionFunctionAbstract $reflection): ?ResolvedResource
+    {
+        return $this->resolveFromReturnType($reflection->getReturnType())
+            ?? $this->resolveFromSource($reflection);
+    }
+
+    private function resolveFromReturnType(mixed $returnType): ?ResolvedResource
+    {
+        if ($returnType instanceof ReflectionNamedType && $this->isResourceClass($returnType->getName())) {
+            return new ResolvedResource($returnType->getName());
+        }
+
+        if ($returnType instanceof ReflectionUnionType) {
+            foreach ($returnType->getTypes() as $type) {
+                if ($type instanceof ReflectionNamedType && !$type->isBuiltin() && $this->isResourceClass($type->getName())) {
+                    return new ResolvedResource($type->getName());
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private function resolveFromSource(ReflectionFunctionAbstract $reflection): ?ResolvedResource
     {
         $fileContent = $this->getFileContent($reflection);
         $code = $this->getFunctionCode($reflection, $fileContent);
@@ -28,11 +54,9 @@ class ResourceClassResolver
                 continue;
             }
 
-            $resourceName = $matches[1];
-
-            $resourceName = class_exists($resourceName)
-                ? $resourceName
-                : $this->getClassNameFromImports($resourceName, $fileContent);
+            $resourceName = class_exists($matches[1])
+                ? $matches[1]
+                : $this->getClassNameFromImports($matches[1], $fileContent);
 
             if (is_subclass_of($resourceName, JsonResource::class)) {
                 return new ResolvedResource($resourceName, $type === 'collection');
@@ -40,6 +64,12 @@ class ResourceClassResolver
         }
 
         return null;
+    }
+
+    private function isResourceClass(string $className): bool
+    {
+        return is_subclass_of($className, JsonResource::class)
+            && $className !== AnonymousResourceCollection::class;
     }
 
     private function getFileContent(ReflectionFunctionAbstract $reflection): array
